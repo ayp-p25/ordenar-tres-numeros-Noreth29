6a46a06070d30b9d4ebd372088a822e1a7f1aa96
Noreth29 <noreth.guajardo@iteso.mx>
2025-02-18 18:28:58 -0600

    intento 2

--- ordenar3.py
+++ ordenar3.py
@@ -11,26 +11,23 @@ E2 = int(input("Segundo número: "))
 E3 = int(input("Tercer número: "))
 
 # Proceso
-if E1 != E2 and E1 != E3 and E2 != E3:
 
-    if E1 > E2 and E2 > E3:
-        salida = str(E1) + " " + str(E2) + " " + str(E3)
-    if E1 > E3 and E3 > E2:
-        salida = str(E1) + " " + str(E3) + " " + str(E2)
 
-    if E2 > E3 and E3 > E1:
-        salida = str(E2) + " " + str(E3) + " " + str(E1)
-    if E2 > E1 and E1 > E3:
-        salida = str(E2) + " " + str(E1) + " " + str(E3)
+if E1 >= E2 and E2 >= E3:
+    salida = str(E1) + " " + str(E2) + " " + str(E3)
+if E1 >= E3 and E3 >= E2:
+    salida = str(E1) + " " + str(E3) + " " + str(E2)
 
-    if E3 > E1 and E1 > E2:
-        salida = str(E3) + " " + str(E1) + " " + str(E2)
-    if E3 > E2 and E2 > E1:
-        salida = str(E3) + " " + str(E2) + " " + str(E1)
+if E2 >= E3 and E3 >= E1:
+    salida = str(E2) + " " + str(E3) + " " + str(E1)
+if E2 >= E1 and E1 >= E3:
+    salida = str(E2) + " " + str(E1) + " " + str(E3)
 
-    print("Números ordenados: " + str(salida))   
+if E3 >= E1 and E1 >= E2:
+    salida = str(E3) + " " + str(E1) + " " + str(E2)
+if E3 >= E2 and E2 >= E1:
+    salida = str(E3) + " " + str(E2) + " " + str(E1)
 
-else:
-    no = "Error. Por favor, escribe números diferentes."
+print("Números ordenados: " + str(salida))   
 
-    print(no)
+
